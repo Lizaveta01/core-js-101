78b23cf18515f70fcd6b8bbcc62b14e8600451a2
Lizaveta Petrova <goldglaider@gmail.com>
2022-07-21 20:46:20 +0300

    feat: add solution array

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -217,9 +217,10 @@ function roundToPowerOfTen(num, pow) {
  */
 
 function isPrime(n) {
-  if (n === 2 || n === 3) return true;
-  if (n % 2 === 0 || n % 3 === 0) return false;
-  return true;
+  for (let i = 2; i < (n / 2) + 1; i = +1) {
+    if (n % i === 0) return false;
+  }
+  return n !== 1;
 }
 
 /**
